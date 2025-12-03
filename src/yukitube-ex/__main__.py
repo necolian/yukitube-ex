e397d8c91a1c630fec6663483680fff301d48a6d
ネコリアン <153508475+necolian@users.noreply.github.com>
2025-12-03 09:13:35 +0900

    Cookieの制限をなくした

--- src/yukitube-ex/__main__.py
+++ src/yukitube-ex/__main__.py
@@ -37,7 +37,10 @@ def home(response: Response,
          yuki: str = Cookie(None)):
     
     if check_cokie(yuki):
-        response.set_cookie("yuki", "True", max_age=60 * 60 * 24 * 7)
+        response.set_cookie("yuki",
+                            "True"#,
+                            #max_age=60 * 60 * 24 * 7
+                           )
         return template("home.html", {"request": request})
 
     print(check_cokie(yuki))
